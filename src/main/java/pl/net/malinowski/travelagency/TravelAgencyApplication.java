f76970a97028f2b49c0323f063b6fee4a7d77978
malinowskij <jaqb_malinowski@yahoo.com>
2017-12-15 20:28:30 +0100

    Initial commit

---
+++ src/main/java/pl/net/malinowski/travelagency/TravelAgencyApplication.java
@@ -0,0 +1,12 @@
+package pl.net.malinowski.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyApplication.class, args);
+	}
+}
