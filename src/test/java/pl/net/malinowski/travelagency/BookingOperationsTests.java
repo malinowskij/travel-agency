1931cf2b9704e724cfcce1c4d807fa557b4c635e
malinowskij <jaqb_malinowski@yahoo.com>
2017-12-30 16:17:13 +0100

    optimizing imports

--- src/test/java/pl/net/malinowski/travelagency/BookingOperationsTests.java
+++ src/test/java/pl/net/malinowski/travelagency/BookingOperationsTests.java
@@ -5,12 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import pl.net.malinowski.travelagency.data.entity.Booking;
-import pl.net.malinowski.travelagency.data.entity.Trip;
-import pl.net.malinowski.travelagency.data.repository.BookingRepository;
 import org.junit.Assert;
 import pl.net.malinowski.travelagency.logic.service.interfaces.BookingService;
-import pl.net.malinowski.travelagency.logic.util.DateUtil;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
